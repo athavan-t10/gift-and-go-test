eb9b8bb4e1584992f238ea35d3f33ca01307d229
athavan-t10 <athavan.thayalan@hotmail.com>
2026-01-07 19:17:23 +0000

    Cleanup

--- src/main/java/giftandgo/GiftAndGoTestApplication.java
+++ src/main/java/giftandgo/GiftAndGoTestApplication.java
@@ -3,10 +3,8 @@ package giftandgo;
 import giftandgo.config.FeatureFlags;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-//@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 @SpringBootApplication
 @EnableConfigurationProperties(FeatureFlags.class)
 public class GiftAndGoTestApplication {
